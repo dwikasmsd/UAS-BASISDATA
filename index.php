c619e49dad348b77692b941c561613d2582facdb
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-05-26 03:27:09 +0700

    membuat halaman login

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<link rel="stylesheet" type="text/css" href="aset/style.css">
+</head>
+<body>
+<div class="container">
+	<div class="login">
+		<div class="header">
+			<h1>Login</h1>
+		</div>
+		<div class="main">
+			<form>
+				<input type="text" placeholder="Username">
+				<br>
+				<input type="password" placeholder="password">
+				<br>
+				<a href="">Forgot My Password</a>
+				<a href="">login as Admin</a>
+				<br>
+				<button>Login</button>
+			</form>
+		</div>
+	</div>
+	<div class="img">
+		<span>
+			<h1>Halo Petani</h1>
+			<p>solusi untuk keluhan para petani di indonesia<br></p>
+		</span>
+	</div>
+</div>
+</body>
+</html>
