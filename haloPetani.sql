b3de9874a5f0dce85ceb2928db5ffc2c1759eba9
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-05-31 10:15:44 +0700

    Menambah file haloPetani.sql

---
+++ haloPetani.sql
@@ -0,0 +1,29 @@
+CREATE TABLE admin (
+
+    id_admin INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nama_admin VARCHAR(260) NOT NULL,
+    password_admin VARCHAR(260) NOT NULL    
+)
+
+CREATE TABLE artikel (
+
+    id_artikel INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    judul_artikel VARCHAR(300) NOT NULL,
+    isi_artikel LONGTEXT NOT NULL,
+    kategori_artikel VARCHAR(300) NOT NULL,
+    tanggal_artikel DATE NOT NULL,
+    rating FLOAT
+)
+
+CREATE TABLE pengguna (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nama_lengkap VARCHAR(260) NOT NULL,
+    no_hp VARCHAR(260) NOT NULL,
+    password VARCHAR(260) NOT NULL,
+    quality_point INT 
+)
+
+CREATE TABLE tanaman (
+    id_tanaman INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    jenis_tanaman VARCHAR(260) NOT NULL 
+)
